eae023fa05c3b5bd34e6dc0ef0509523f3b01733
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2013-09-22 21:12:08 -0400

    wsgi and Procfile update

--- datacademy/datacademy/wsgi.py
+++ datacademy/datacademy/wsgi.py
@@ -30,8 +30,12 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "datacademy.settings.production"
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+# application = get_wsgi_application()
+application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
+
+
