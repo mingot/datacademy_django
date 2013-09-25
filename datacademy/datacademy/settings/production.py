cbe6849c9443e8a4f0403c1d55112bd479abf93d
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2013-09-25 10:39:06 -0400

    debugging in production...

--- datacademy/datacademy/settings/production.py
+++ datacademy/datacademy/settings/production.py
@@ -22,6 +22,7 @@ def get_env_setting(setting):
 # See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
 # ALLOWED_HOSTS = []
 ########## END HOST CONFIGURATION
+DEBUG = True
 
 ########## EMAIL CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
